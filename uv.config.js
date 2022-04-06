c412896fe9059fc32096544d134497b2f5617fa4
LokiLy3eu <100978455+LokiLy3eu@users.noreply.github.com>
2022-04-05 18:56:07 -0800

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,11 +1,9 @@
-{
-	"versions": [
-		"v1"
-	],
-	"language": "NodeJS",
-	"memoryUsage": 11.21,
-	"developer": {
-		"name": "TOMPHTTP NodeJS Bare Server",
-		"repository": "https://github.com/tomphttp/bare-server-node"
-	}
-}
+self.__uv$config = {
+    bare: '/bare/',
+    prefix: '/service/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv.handler.js',
+    bundle: '/uv.bundle.js',
+    config: '/uv.config.js',
+};
